923b481045719f8ab8255c9a78bcd863d0c28e92
Petr Kulhanek <kulhanek@chemi.muni.cz>
2022-10-08 12:14:18 +0200

    update

--- 11.build-final-wolf-server.sh
+++ 11.build-final-wolf-server.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
 PREFIX=/opt/wolf-stat-server/3.0
+DESDIR=/export/packages/22.04/src/packages/wolf/ncbr-wolf-stat-server
 
 set -o pipefail
-
-if [[ "`hostname -f`" != "deb8.ncbr.muni.cz" ]]; then
-    echo "unsupported build machine - it must be deb8.ncbr.muni.cz!"
-    exit 1
-fi
 
 # add cmake
 module add cmake git
@@ -34,6 +30,6 @@ set -o pipefail
 
 cmake -DCMAKE_INSTALL_PREFIX="$PREFIX" -DSTAT_TARGET=server . 2>&1 | tee configure.log || exit 1
 make -j "$N" 2>&1 | tee make.log || exit 1
-make install 2>&1 | tee install.log || exit 1
+make DESTDIR=$DESDIR install 2>&1 | tee install.log || exit 1
 
 echo ""
